2d9475a12e62bc7a7ec69ef412f2e84b91ecd92c
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-04-01 14:40:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ const api = fetch("http://localhost:3000/videos")
 .then(res => res.json())
 .then((videos) =>
         videos.forEach((video) => {
-            containerVideos.innerHTML += 
+            containerVideos.innerHTML += `
+            <li class="videos__item">
+            <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+            <div class="descricao-video">
+                <img class="img-canal" src="${video.imagem}" alt="Logo do Canal"></img>
+                <h3 class="titulo-video">${video.titulo}</h3>
+                <p class="titulo-canal">${video.descricao}</p>
+            </div>
+            </li>
+            `;
     })
 )
+.catch((error) => {
+    containerVideos.innerHTML = `<p>Erro inesperado ao carregar, tente novamente mais tarde, Erro: ${error}</p>`
+})
